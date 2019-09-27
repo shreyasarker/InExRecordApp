849ddf66a5b9f231f86fc5c422932c13431de1a6
Md Imrul Kayes <imrul.diu@gmail.com>
2019-09-27 16:45:59 +0600

    Annotation Added in Income and Expense Models

--- InExRecordApp/Models/Income.cs
+++ InExRecordApp/Models/Income.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +9,31 @@ namespace InExRecordApp.Models
 {
     public class Income
     {
+        [Key]
         public int Id { get; set; }
+        [Column(TypeName = "int")]
+        [Required]
         public int UserId { get; set; }
+        [Column(TypeName = "varchar(20)")]
+        [Required]
         public string IncomeType { get; set; }
-        public double Amount { get; set; }
+        [Column(TypeName = "float(20)")]
+        [Required]
+        public decimal Amount { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        [Required]
         public string ChequeNo { get; set; }
+        [Column (TypeName = "varchar(100)")]
+        [Required]
         public string BankName { get; set; }
+        [Column(TypeName = "text")]
+        [Required]
         public string Particular { get; set; }
+        [Column(TypeName = "datetime")]
+        [Required]
         public DateTime Date { get; set; }
-        public bool IsApproved { get; set; }
+        [Column(TypeName = "int")]
+        [Required]
+        public int IsApproved { get; set; }
     }
 }
